8fd0e14b88a48bbd8b02ee5f26daa875f874af49
Giuseppe De Marco <g.demarco@eurolink.it>
2026-03-03 12:24:36 +0100

    Added pathlib for resolve .env references

--- BE/app/core/config.py
+++ BE/app/core/config.py
@@ -1,14 +1,19 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+ROOT_DIR = Path(__file__).resolve().parents[3]
 
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    MODEL_PATH: str | None = "C:/Users/giuse/Desktop/FANKEE_code_challenge/AI-Mission-Generator/models/gemma-3n-E2B-it-Q4_0.gguf"
+    MODEL_PATH: str | None = str(ROOT_DIR / "models" / "gemma-3n-E2B-it-Q4_0.gguf")
     APP_HOST: str = "127.0.0.1"
     APP_PORT: int = 8001
 
     model_config = SettingsConfigDict(
-        env_file=".env",
+        env_file=str(ROOT_DIR / ".env"),
         extra="ignore",
     )
 
