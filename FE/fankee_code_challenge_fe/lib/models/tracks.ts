b3e511ec8873b2424d6049ea879c236ef7e31191
Giuseppe De Marco <giuseppedemarco2004@virgilio.it>
2026-03-03 23:57:05 +0100

    Added Tracks DTO

---
+++ FE/fankee_code_challenge_fe/lib/models/tracks.ts
@@ -0,0 +1,13 @@
+export type TrackRead = {
+  id: number;
+  artist_name: number;
+  track_title: string | null;
+  track_description: string | null;
+  created_at: string | null;
+  updated_at: string | null;
+  deleted_at: string | null;
+};
+
+export type TrackVerifyPayload = {
+  artist_name?: number;
+};
