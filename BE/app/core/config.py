5a18d418d4bee6c2a22ae543b2719f56476fdd9c
Giuseppe De Marco <giuseppedemarco2004@virgilio.it>
2026-03-03 03:15:53 +0100

    Added AI_MISSION_GENERATOR_URL for Ai Mission generator

--- BE/app/core/config.py
+++ BE/app/core/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL="mysql+pymysql://root:password@localhost:3306/fankee_code_challenge"
+    AI_MISSION_GENERATOR_URL: str = "http://localhost:8000"
 
     model_config = SettingsConfigDict(
         env_file=".env",
