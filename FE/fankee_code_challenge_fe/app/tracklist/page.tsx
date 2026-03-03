e982e99171caf2f7f49b2f1566b88e492086604d
Giuseppe De Marco <giuseppedemarco2004@virgilio.it>
2026-03-04 00:26:30 +0100

    Added Tracklist page

---
+++ FE/fankee_code_challenge_fe/app/tracklist/page.tsx
@@ -0,0 +1,11 @@
+export default function Tracklist() {
+  return (
+    <main className="flex min-h-screen items-start justify-center bg-gradient-to-b from-[#6f6500] via-[#141300] to-black px-5 pt-8">
+        <div className="flex h-full items-start justify-center pt-20">
+          <h1 className="text-[32px] text-[#ffe600]">
+            Choose your track
+          </h1>
+      </div>
+    </main>
+  );
+}
