d01d582dbda9fa46791883c5fa6e949cb11d08d1
Giuseppe De Marco <giuseppedemarco2004@virgilio.it>
2026-03-03 02:39:25 +0100

    Added GET request to get users and verify existing user

--- BE/app/api/v1/users.py
+++ BE/app/api/v1/users.py
@@ -7,3 +7,15 @@ router = APIRouter()
 @router.get("/{user_id}", response_model=UserDTO)
 def get_user(user_id: int, service: UserService = Depends()):
     return service.get_by_id(user_id)
+
+@router.get("/", response_model=list[UserDTO])
+def get_all_users(service: UserService = Depends()):
+    return service.get_all_users()
+
+@router.post("/", response_model=UserDTO)
+def create_user(payload: UserCreate, service: UserService = Depends()):
+    return service.create_user(payload)
+
+@router.post("/verify", response_model=UserDTO)
+def verify_existing_user(nickname: str, service: UserService = Depends()):
+    return service.verify_existing_user(nickname)
