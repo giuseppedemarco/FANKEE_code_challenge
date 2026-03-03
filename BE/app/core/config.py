16d5de033835df0bd188769277aaf7bf9cbdfaa9
Giuseppe De Marco <giuseppedemarco2004@virgilio.it>
2026-03-03 02:41:07 +0100

    Added DB config

---
+++ BE/app/core/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DATABASE_URL=mysql+pymysql://root:password@localhost:3306/fankee_code_challenge
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore",
+    )
+
+
+settings = Settings()
