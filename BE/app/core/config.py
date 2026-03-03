166b5e455a72c09e6848163941c96251d7ba7728
Giuseppe De Marco <g.demarco@eurolink.it>
2026-03-03 09:49:50 +0100

    Fix: removed  AI_MISSION_GENERATOR_URL

--- BE/app/core/config.py
+++ BE/app/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL="mysql+pymysql://root:password@localhost:3306/fankee_code_challenge"
-    AI_MISSION_GENERATOR_URL: str = "http://localhost:8000"
+    MODEL_PATH: str | None = "C:/Users/giuse/Desktop/FANKEE_code_challenge/AI-Mission-Generator/models/gemma-3n-E2B-it-Q4_0.gguf"
 
     model_config = SettingsConfigDict(
         env_file=".env",
