e24c2d0a2c7218404a08640c03724bea84931c1f
Giuseppe De Marco <g.demarco@eurolink.it>
2026-03-03 09:47:05 +0100

    Added router for missions

--- BE/app/api/v1/router.py
+++ BE/app/api/v1/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from app.api.v1 import users, tracks
+from app.api.v1 import missions, tracks, users
 
 router = APIRouter()
 router.include_router(users.router, prefix="/users", tags=["users"])
 router.include_router(tracks.router, prefix="/tracks", tags=["tracks"])
+router.include_router(missions.router, prefix="/missions", tags=["missions"])
