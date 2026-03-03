ab37f2a1c3934a338ccd407d3d07e6ed73c9b23c
Giuseppe De Marco <g.demarco@eurolink.it>
2026-03-03 10:05:49 +0100

    FIX: DATABASE_URL

--- BE/app/core/config.py
+++ BE/app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DATABASE_URL="mysql+pymysql://root:password@localhost:3306/fankee_code_challenge"
+    DATABASE_URL: str = "mysql+pymysql://root:password@localhost:3306/fankee_code_challenge"
     MODEL_PATH: str | None = "C:/Users/giuse/Desktop/FANKEE_code_challenge/AI-Mission-Generator/models/gemma-3n-E2B-it-Q4_0.gguf"
 
     model_config = SettingsConfigDict(
