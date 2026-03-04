ca9d54f9d43ab469cfa19d05f7def8d955a2492d
Giuseppe De Marco <g.demarco@eurolink.it>
2026-03-04 11:19:17 +0100

    Added imageSrc to Missions() component

--- FE/fankee_code_challenge_fe/app/tracklist/missions/page.tsx
+++ FE/fankee_code_challenge_fe/app/tracklist/missions/page.tsx
@@ -8,14 +8,20 @@ export default function Missions() {
   const artist = searchParams.get("artist") ?? "Unknown artist";
   const title = searchParams.get("title") ?? "Untitled track";
   const genre = searchParams.get("genre") ?? "Unknown genre";
+  const imageSrc = searchParams.get("imageSrc") ?? "Unknown image src";
 
   return (
-    <div className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#6f6500] via-[#141300] to-black px-5 pt-8">
-      <div className="mt-16 text-center">
+    <div className="relative flex min-h-screen flex-col items-center bg-gradient-to-b from-[#6f6500] via-[#141300] to-black px-5 pt-8">
+      <div className="z-[20] mt-16 text-center">
         <p className="text-lg text-[#ffe600]">{genre}</p>
         <p className="mt-2 text-4xl font-semibold text-white">{title}</p>
         <p className="mt-2 text-2xl text-[#ffe600]">{artist}</p>
       </div>
+
+      <div
+        className="absolute left-0 top-0 z-0 h-screen w-full bg-left bg-cover bg-no-repeat opacity-35"
+        style={{ backgroundImage: `url(${imageSrc})` }}
+      />
     </div>
   );
 }
